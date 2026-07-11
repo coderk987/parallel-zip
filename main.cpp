53bf09bb0ee30f7230dec3c42217f5957502d61a
Kunsh <k789.tyagi@gmail.com>
2026-07-11 09:27:48 +0530

    tree creation test

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode{
+    char val;
+    TreeNode *right;
+    TreeNode *left;
+};
+
+vector<pair<int, char>> txtdata;
+
+TreeNode* makeTree(int l,int r){
+    if(l==r){
+        TreeNode *node = new TreeNode;
+        node->val = txtdata[l].second;
+        node->right = nullptr;
+        node->left = nullptr;
+        return node;
+    }
+
+    int mid = (l+r)/2; //temporary incorrect split
+    TreeNode *root = new TreeNode;
+    root->val = '*'; //temporary
+    root->left = makeTree(l, mid);
+    root->right = makeTree(mid+1, r);
+    return root;
+}
+
+void dfs(TreeNode *root){
+    if(root==nullptr) return;
+
+    cout<<root->val<<" ";
+    dfs(root->right);
+    dfs(root->left);
+}
+
+int main(){
+    string s = "hellowowowo";
+
+    vector<int> freq(256,0);
+    for(char &c: s){
+        freq[static_cast<int>(c)]++;
+    }
+
+    for(int i=0;i<256;i++){
+        if(freq[i]>0){
+            pair<int,int> p = {freq[i], static_cast<char>(i)};
+            txtdata.push_back(p);
+        }
+    }
+    
+    sort(txtdata.begin(), txtdata.end());
+    for(auto &x: txtdata){
+        cout<<x.second<<"=>"<<x.first<<endl;
+    }
+
+    TreeNode *root = makeTree(0, txtdata.size()-1);
+
+    dfs(root);
+
+    return 0;
+}
